ca2b4e82ce26dc38d89e2072cce99f234d4d1227
vladislav-deviant <derzhavin19@mail.ru>
2019-09-20 02:06:26 +0600

    Updates

---
+++ precache-manifest.7fcba70eefe0944c874589870538f49c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafbc554f46bb4699abd7804f3df2d2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "456333ef0f8f4621b59e",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "456333ef0f8f4621b59e",
+    "url": "./static/js/2.77753224.chunk.js"
+  },
+  {
+    "revision": "7d4695ef95131e5a4a59",
+    "url": "./static/js/main.e955582a.chunk.js"
+  },
+  {
+    "revision": "393ae6dfe3716e8bc4ae",
+    "url": "./static/js/runtime-main.c0f600a8.js"
+  }
+]);
